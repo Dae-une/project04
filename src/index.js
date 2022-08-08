652a7d4ee626e38dc476c47a13a9878be921b56a
Dae-une <ko301201@naver.com>
2022-08-08 15:59:00 +0900

    add coockie and refatoring with Thunk

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalStyle } from "./GlobalStyles";
+import { CookiesProvider } from "react-cookie";
+import { Provider } from "react-redux";
+import store from "./redux/config/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <Provider store={store}>
+      <CookiesProvider>
+        <App />
+        <GlobalStyle />
+      </CookiesProvider>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -10,21 +10,25 @@ import {
   LinkContainer,
 } from "../SignUp/styles";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import useInput from "../../hooks/useInput";
+import { useCookies } from "react-cookie";
+import { getUsersAysnc } from "../../redux/modules/signupSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 const LogIn = () => {
-  const [userData, setUserData] = useState("");
   const [userId, onChangeuUserId, setUserId] = useInput("");
   const [password, onChangePassword, setPassword] = useInput("");
   const [logInError, setLogInError] = useState(false);
+  const [cookies, setCookie] = useCookies(["garbageCookie"]);
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    axios
-      .get(process.env.REACT_APP_API_USERS)
-      .then((res) => setUserData(res.data));
+    dispatch(getUsersAysnc());
   }, []);
+
+  const userData = useSelector((state) => state.data);
 
   const onSubmit = useCallback(
     (e) => {
@@ -34,6 +38,10 @@ const LogIn = () => {
         userData.findIndex((user) => user.userId === userId) > -1 &&
         userData.findIndex((user) => user.password === password) > -1
       ) {
+        setCookie("garbageCookie", userId, {
+          path: "/",
+          maxAge: 1000 * 60 * 60,
+        });
         navigate("/");
       } else {
         setLogInError(true);
